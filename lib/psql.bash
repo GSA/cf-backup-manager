d37b1b190ba9efbc59d93c73c04be30d5297ed70
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-12-08 16:13:18 -0500

    new: change db_flags from cmd argument to env variable

--- lib/psql.bash
+++ lib/psql.bash
@@ -3,7 +3,8 @@
 # Output service credentials as env variables
 function service_get_credentials_env () {
   local service_name="${1:-}"
-  [[ -z "$service_name" ]] && fail service_get_credentials_env passed an empty service_name
+  [[ -z "$service_name" ]] && fatal service_get_credentials_env passed an empty service_name
+  [[ -z "$(get_service_instance "$service_name")" ]] && fatal $service_name does not exist in vcap_services
 
   cat <<EOF
 DB_HOST="$(get_service_instance "$service_name" | jq -r -e '.credentials.host')"
@@ -18,10 +19,10 @@ EOF
 # https://github.com/GSA/datagov-deploy/issues/2788#issuecomment-983806227
 function service_backup () {
   # Custom backup flags passed in as $1
-  PGPASSWORD=$DB_PASSWORD pg_dump -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" --format=custom $1 "$DB_NAME"
+  PGPASSWORD=$DB_PASSWORD pg_dump -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" --format=custom --no-owner ${PG_DUMP_OPTIONS:-} "$DB_NAME"
 }
 
 function service_restore () {
   # Custom restore flags passed in as $1
-  PGPASSWORD=$DB_PASSWORD pg_restore -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" $1 -d "$DB_NAME"
+  PGPASSWORD=$DB_PASSWORD pg_restore -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" --no-owner ${PG_RESTORE_OPTIONS:-} -d "$DB_NAME"
 }
